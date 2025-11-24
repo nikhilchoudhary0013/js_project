d269e5d27bcf4c31443f6c99d68c174171e2cc02
Nikhil Choudhary <nikhilchoudhary2242@gmail.com>
2025-11-24 15:03:06 +0530

    operators program

---
+++ oper.js
@@ -0,0 +1,13 @@
+// Airthmetic op (+ ,- ,* ,/ ,%)
+let a = 100 + 300;
+console.log(a);
+
+let b = 300 - 100;
+console .log(b);
+
+let c = 100 * 5 ;
+console .log(c);
+
+let d = 10/5;
+console.log(d);
+
