b5f5eacb6e8d719419850b87464897260600f086
Nikhil Choudhary <nikhilchoudhary2242@gmail.com>
2025-11-27 14:17:34 +0530

    this is function file

---
+++ fun.js
@@ -0,0 +1,21 @@
+// parameter function 
+
+function para(a1,b1){
+    return console.log(a1 + b1);
+}
+para(25,25);
+
+// Function Expressions
+let a = function(b , c){
+    return console.log(b + c);
+}
+let a1 = a(25,25);
+
+// Arrows function
+let Mul = (g , h)=>{
+    console.log(g * h);
+}
+Mul(2,2);
+
+
+
