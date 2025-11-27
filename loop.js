c0df00e209009abcd9a8a770851f1d4852ffc1d6
Nikhil Choudhary <nikhilchoudhary2242@gmail.com>
2025-11-27 14:16:21 +0530

    this is loops example file

---
+++ loop.js
@@ -0,0 +1,37 @@
+let i = 0;
+for(let i =0; i<10;i++){
+    console.log(i)
+} 
+
+let a = 0;
+while(a <= 10){
+    console.log(a);
+    a++;
+}
+
+let b =0;
+do{
+    console.log(b)
+    b++;
+}
+while(b<=10);
+
+// string 
+let name = "nikhil";
+console.log(name);
+
+// string template
+let name1 = "nikhil";
+let lastname = "choudhary";
+
+console.log(`good morning ${name1}`);
+
+// Object
+
+const stu ={
+    s_name: "nikhil",
+    l_name:"choudhary",
+    age:20,
+
+};
+console.log(stu .s_name);
