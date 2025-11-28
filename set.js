8fb8b743f436348016dbbbcc2b3c608dd3b10cfa
Nikhil Choudhary <nikhilchoudhary2242@gmail.com>
2025-11-28 15:03:14 +0530

    Array,Map and Set example files

---
+++ set.js
@@ -0,0 +1,18 @@
+const se = new Set(["apple","boy","cat","dog"]);
+
+se.add("eye");
+se.add("fish");
+se.add("gun")
+se.delete("apple");
+se.add("apple");
+se.add("boy");
+
+ console.log(se);
+
+ const se1 = new Set(["Alto",
+    "BMW",
+    "Swift",
+    "OD"
+ ]);
+ se1.clear();
+ console.log(se1);
